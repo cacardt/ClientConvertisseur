b89f8af125248b80acbad851a50ed441378b134b
cacardt <theo.cacard@etu.univ-savoie.fr>
2023-01-25 09:23:03 +0100

    Tout

---
+++ ClientConvertisseurV1/Models/Devise.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientConvertisseurV1.Models
+{
+    internal class Devise
+    {
+		private int id;
+		private string? nomDevise;
+		private double taux;
+
+		public double Taux
+		{
+			get { return taux; }
+			set { taux = value; }
+		}
+
+
+		public string? NomDevise
+		{
+			get { return nomDevise; }
+			set { nomDevise = value; }
+		}
+
+
+		public int Id
+		{
+			get { return id; }
+			set { id = value; }
+		}
+
+	}
+}
---
+++ ClientConvertisseurV1/Services/WSService.cs
@@ -0,0 +1,37 @@
+﻿using ClientConvertisseurV1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientConvertisseurV1.Services
+{
+    internal class WSService : IService
+    {
+        public HttpClient client = new HttpClient();
+        public WSService()
+        {
+            client.BaseAddress = new Uri("http://localhost:7223/api");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<List<Devise>> GetDevisesAsync(string nomControleur)
+        {
+            try
+            {
+                return await client.GetFromJsonAsync<List<Devise>>(nomControleur);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+
+        }
+    }
+}
